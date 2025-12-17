8947a71a6b9dd3955fec0445f4f9c26712d34090
Vidura Vijerathne <85375880+ViduraVijerathne@users.noreply.github.com>
2025-12-17 17:35:18 +0530

    add dto abstract class

---
+++ src/main/java/com/vidura/exam/dto/DTO.java
@@ -0,0 +1,16 @@
+package com.vidura.exam.dto;
+
+import com.vidura.exam.dto.response.ServerStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public abstract class DTO {
+    private String message;
+    private ServerStatus status;
+
+}
---
+++ src/main/java/com/vidura/exam/dto/GlobalDTO.java
@@ -0,0 +1,13 @@
+package com.vidura.exam.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GlobalDTO extends DTO{
+    private int statusCode;
+}
